9439f7163f0335c9e6161d44e1d3f61dfd863ce5
Muhammad Sarim Khan Bangash <bangashsarim@gmail.com>
2021-07-02 02:45:17 +0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Card from "./components/Card";
+import Sdata from "./Sdata";
 
 function App() {
+  // console.log(Sdata[0]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="heading_style">List of top 5 Netflix Series in 2021</h1>
+      {Sdata.map((value, index) => {
+        return (
+          <Card
+            key={index}
+            imgsrc={value.imgsrc}
+            title={value.title}
+            sname={value.sname}
+            link={value.link}
+          />
+        );
+      })}
+      ;
+    </>
   );
 }
 
---
+++ src/Sdata.jsx
@@ -0,0 +1,37 @@
+const Sdata = [
+  {
+    sname: "DARK",
+    imgsrc: "https://wallpapercave.com/wp/wp4056410.jpg",
+    title: "A Netflix Original Series",
+    link: "https://www.netflix.com",
+  },
+  {
+    sname: "Extra Curricular",
+    imgsrc:
+      "https://m.media-amazon.com/images/M/MV5BNGQxMDcyOTEtZWZkNi00NzgwLWEzNjQtZmFhMGY5ZGRlMTdiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
+    title: "A Netflix Original Series",
+    link: "https://www.netflix.com",
+  },
+  {
+    sname: "Stranger Things",
+    imgsrc:
+      "https://images-na.ssl-images-amazon.com/images/I/71OB1IywjLL._AC_SY679_.jpg",
+    title: "A Netflix Original Series",
+    link: "https://www.netflix.com",
+  },
+  {
+    sname: "The Vampire Diaries",
+    imgsrc:
+      "https://i.pinimg.com/originals/7e/59/3f/7e593fe7f57f3eb8245b91c440d3a226.jpg",
+    title: "A Netflix Original Series",
+    link: "https://www.netflix.com",
+  },
+  {
+    sname: "My First-2 Love",
+    imgsrc: "https://i.mydramalist.com/2OEvwc.jpg?v=1",
+    title: "A Netflix Original Series",
+    link: "https://www.netflix.com",
+  },
+];
+
+export default Sdata;
